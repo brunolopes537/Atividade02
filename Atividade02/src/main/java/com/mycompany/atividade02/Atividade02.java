159221df60caa852fbaf3e64fdd618c0793b2d4f
brunolopes537 <brunolopes537@gmail.com>
2025-08-29 16:57:00 -0300

    Add files via upload
    Atividade 2 de Aplicações de Linguagem de Programação Orientada a Objetos.

---
+++ Atividade02/src/main/java/com/mycompany/atividade02/Atividade02.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade02;
+
+
+/**
+ *
+ * @author BRUNO
+ */
+public class Atividade02 {
+
+    public static void main(String[] args) {
+        NewJFrame janela = new NewJFrame(); 
+        janela.setVisible(true);
+
+    }
+}
